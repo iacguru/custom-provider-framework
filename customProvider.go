3b4af46506202ef21292b1082eac4710329db653
iacguru <infraascodeguru@gmail.com>
2022-02-13 16:36:37 -0600

    initial code

---
+++ customProvider.go
@@ -0,0 +1,15 @@
+package cpf
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// Provider -
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema:               CustomSchema.Schema(),
+		ResourcesMap:         CustomResources.Resources(),
+		DataSourcesMap:       CustomResources.DataSources(),
+		ConfigureContextFunc: providerConfigure,
+	}
+}
