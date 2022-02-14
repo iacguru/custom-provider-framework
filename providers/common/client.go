098cd0730b44992667c9e5a77f894be1fd1d8db4
iacguru <infraascodeguru@gmail.com>
2022-02-14 00:33:28 -0600

    initial commit

--- providers/common/client.go
+++ providers/common/client.go
@@ -2,20 +2,28 @@ package common
 
 import (
 	"context"
-	"github.com/google/go-github/github"
 	"github.com/hashicorp-demoapp/hashicups-client-go"
 	"golang.org/x/oauth2"
+	"io"
+	"net/http"
 )
 
-type Client struct{}
+type Client struct {
+	HttpClient *http.Client
+	Method     string
+	URL        string
+	Body       io.Reader
+	Headers    *map[string]string
+	Req        *http.Request
+}
 
-func (c *Client) GitHubNewClient(pat *string) *github.Client {
+func (c *Client) GitHubNewClient(pat string) *http.Client {
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: *pat},
+		&oauth2.Token{AccessToken: pat},
 	)
-	tc := oauth2.NewClient(ctx, ts)
-	return github.NewClient(tc)
+	c.HttpClient = oauth2.NewClient(ctx, ts)
+	return c.HttpClient
 }
 
 func (c *Client) HashiCupsClient(host, username, password *string) (*hashicups.Client, error) {
@@ -24,4 +32,22 @@ func (c *Client) HashiCupsClient(host, username, password *string) (*hashicups.C
 		return nil, err
 	}
 	return newClient, nil
+}
+
+func (c *Client) NewRequest() (*http.Request, error) {
+	var err error
+	c.Req, err = http.NewRequest(c.Method, c.URL, c.Body)
+	if err != nil {
+		return nil, err
+	}
+	return c.Req, nil
+}
+
+func (c *Client) DoRequest() (*http.Response, error) {
+	client := c.HttpClient
+	res, err := client.Do(c.Req)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
